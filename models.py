6afba547d985a294ed2bc69302fe1fdb4a8ea1af
Szymon Misztal <sz.misztal@gmail.com>
2023-04-24 14:19:43 +0200

    add models.py file

---
+++ models.py
@@ -0,0 +1,7 @@
+class Product():
+    def __init__(self, name, quantity, unit, unit_price):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+        self.unit_price = unit_price
+
